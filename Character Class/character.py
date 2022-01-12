4150f9a9f71765a3c3e6e168aabcfec3841500cf
Zensify <78502455+Zensify@users.noreply.github.com>
2022-01-12 11:49:26 -0700

    Update character.py

--- Character Class/character.py
+++ Character Class/character.py
@@ -1,24 +1,23 @@
-class character(object):
+class Character(object):
     def __init__(self, name, catch_phrase, taunt):
         self.name = name
-        self.level = 1
+        self.level = 0
         self.catch_phrase = catch_phrase
         self.taunt = taunt
 
+    def speak(self, speech):
+        if speech == 1:
+            print(self.catch_phrase) 
+        elif speech == 2:
+            print(self.taunt)
 
-Kung_Fu_Panda = character("Master Ping Xiao Po The",
-                          "Skadoosh", "You have been blinded by pure awesomeness!")
-Spider_Man = character("Peter Parker", "My Spider-Sense is tingling",
-                       "Your friendly neighbourhood spiderman.")
+    def setLevel(self, level):
+        self.level == level
+        print(level)
+   
+Kung_Fu_Panda = Character("Master Ping Xiao Po The", "Skadoosh", "You have been blinded by pure awesomeness!")
+Spider_Man = Character("Peter Parker", "My Spider-Sense is tingling", "Your friendly neighbourhood spiderman.")
 
-
-# Catch-phrase
-print(Spider_Man.catch_phrase)
-
-# Change character level
-if Kung_Fu_Panda.level == 1:
-    Kung_Fu_Panda.level = 2
-    print(f"Character is level {Kung_Fu_Panda.level}")
-
-# taunt
-print(Kung_Fu_Panda.taunt)
+Kung_Fu_Panda.speak(2)
+Kung_Fu_Panda.setLevel(2)
+Spider_Man.speak(1)
