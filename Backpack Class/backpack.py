2552ab10bfb25bed57f4b89a5ef60d75b0688687
Zensify <78502455+Zensify@users.noreply.github.com>
2022-01-12 11:41:43 -0700

    Update backpack.py

--- Backpack Class/backpack.py
+++ Backpack Class/backpack.py
@@ -1,40 +1,41 @@
 backpackItem = []
 
-class backpack(object):
+
+class Backpack(object):
     def __init__(self, color, size, item):
-        self.color = color 
-        self.size = size 
-        self.item = item 
+        self.color = color
+        self.size = size
+        self.item = item
         self.open = False
 
     def openBag(self):
         if self.open == False:
             self.open = True
-    
+
     def closeBag(self):
         if self.open == True:
             self.open = False
 
-    def putIn(self):
+    def putIn(self, item):
         if self.open == True:
-            backpackItem.append("lunch") 
-            backpackItem.append("jacket")
-    
-    def takeOut(self):
+            backpackItem.append(item)
+
+    def takeOut(self, item):
         if self.open == True:
-            backpackItem.pop(1)
+            backpackItem.remove(item)
 
 
-#task 2
-blue = backpack("blue", "small", "x")
-red = backpack("red", "medium", "x")
-green = backpack("green", "large", "x")
+# task 2
+blue = Backpack("blue", "small", "x")
+red = Backpack("red", "medium", "x")
+green = Backpack("green", "large", "x")
 
-#blue.openBag() #Open Bag
-blue.putIn() #Put in lunch and jacket
-blue.closeBag() #close 
-#blue.openBag() #open
-blue.takeOut() #take out jacket
-blue.closeBag() #close
+blue.openBag()  # Open Bag
+blue.putIn("lunch")  # Put in Lunch
+blue.putIn("jacket")  # Put in Jacket
+blue.closeBag()  # close
+blue.openBag()  # open
+blue.takeOut("jacket")  # take out jacket
+blue.closeBag()  # close
 
 print(backpackItem)
